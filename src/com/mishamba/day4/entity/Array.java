f55d9df9664a17ba6ec924d4bc941b0b0f1a0736
Mishamba <misha.nenahov@gmail.com>
2020-06-22 09:20:11 +0300

    added second part of task
    rewrote some in first part of task

--- src/com/mishamba/day4/entity/Array.java
+++ src/com/mishamba/day4/entity/Array.java
@@ -1,12 +1,18 @@
 package com.mishamba.day4.entity;
 
 import com.mishamba.day4.exception.ProgramException;
+import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.Stream;
 
 public class Array {
-    private final int[] array;
+    private int[] array;
 
     public Array(int n) {
         this.array = new int[n];
@@ -99,12 +105,65 @@ public class Array {
         intervalQuicksort(left, middle);
     }
 
-    public void sortByMergeSort() {
-        // TODO: 6/20/20
+    public void sortByMergeSort() throws ProgramException {
+        if (this == null) {
+            throw new ProgramException("no object created");
+        }
+        this.array = sort(this.array);
     }
 
-    public void sortByInsertation() {
-        // TODO: 6/20/20  
+    @Contract("_ -> param1")
+    private int @NotNull [] sort(int @NotNull [] givenArray) {
+        if (givenArray.length == 1) {
+            return givenArray;
+        }
+
+        int middle = givenArray.length / 2;
+
+        int[] leftArray = new int[middle];
+        int[] rightArray = new int[middle +1];
+
+
+
+        leftArray = sort(leftArray);
+        rightArray = sort(rightArray);
+
+        return merge(leftArray, rightArray);
+    }
+
+    @Contract(pure = true)
+    private int @NotNull [] merge(int @NotNull [] leftArray, int @NotNull [] rightArray) {
+        int[] mergedArray = new int[leftArray.length + rightArray.length];
+        int positionLeft = 0;
+        int positionRight = 0;
+
+        for (int i = 0;i<mergedArray.length;i++) {
+            if ((positionRight == rightArray.length) ||
+                    (leftArray[positionLeft] < rightArray[positionRight])) {
+                mergedArray[i] = leftArray[positionLeft];
+                positionLeft++;
+            } else {
+                mergedArray[i] = rightArray[positionRight];
+                positionRight++;
+            }
+        }
+
+        return mergedArray;
+    }
+
+    public void sortByInsert() {
+        int key;
+        for (int i = 1; i < array.length; i++) {
+            key = array[i];
+            int j = i - 1;
+
+            while (j >= 0 && key < array[j]) {
+                array[j + 1] = array[j];
+                j--;
+            }
+
+            array[j + 1] = key;
+        }
     }
 
     public int min() {
@@ -130,21 +189,21 @@ public class Array {
     }
 
     public int[] findSingleNumbers() {
-        Reporter func = this::isSingle;
-        return findElems(func);
+        Reporter func = this::single;
+        return findElements(func);
     }
 
     public int[] findFibonacciNumbers() {
-        Reporter func = this::isFibonacci;
-        return findElems(func);
+        Reporter func = this::fibonacci;
+        return findElements(func);
     }
 
     public int[] findNumbersWithNoSameLetters() {
         Reporter func = this::noSameLetters;
-        return findElems(func);
+        return findElements(func);
     }
 
-    private boolean isSingle(int elem) {
+    private boolean single(int elem) {
         double mark = 0;
         for (int j = elem; j > 0; j--) {
             mark += ((double) elem) / j;
@@ -153,10 +212,10 @@ public class Array {
         return (mark == 0);
     }
 
-    private boolean isFibonacci(int elem) {
+    private boolean fibonacci(int elem) {
         int MAX_FIBONACCI_NUMBER = 47;
         for (int i = 0; i < MAX_FIBONACCI_NUMBER; i++) {
-            if (elem == fibonacci(i)) {
+            if (elem == calculateFibonacci(i)) {
                 return true;
             }
         }
@@ -164,34 +223,66 @@ public class Array {
         return false;
     }
 
-    private int fibonacci(int n) {
+    private int calculateFibonacci(int n) {
         double phi = (Math.sqrt(5) + 1) / 2;
         return (int) (Math.pow(phi, n) / Math.sqrt(5) + 0.5);
     }
 
     private boolean noSameLetters(int elem) {
-        int firstNumber = elem % 10;
-        int secondNumber = (elem % 100 - elem % 10) / 10;
-        int thirdNumber = (elem - secondNumber * 10 - firstNumber) / 100;
-        return (secondNumber == thirdNumber) && (firstNumber == thirdNumber);
+        int unity = elem % 10;
+        int ten = (elem % 100 - unity) / 10;
+        int century = (elem - ten * 10 - unity) / 100;
+        return (ten == century) && (unity == century);
     }
 
-    private int @NotNull [] findElems(Reporter func) {
+    private int @NotNull [] findElements(Reporter func) {
         int quantity = 0;
         for (int elem : array) {
-            quantity += (func.apear(elem)) ? 1 : 0;
+            quantity += (func.appear(elem)) ? 1 : 0;
         }
 
         int[] numbers = new int[quantity];
         int j = 0;
         for (int elem : array) {
-            if (func.apear(elem)) {
+            if (func.appear(elem)) {
                 numbers[j] = elem;
                 j++;
             }
         }
 
         return numbers;
+    }
+
+    public void fillArrayByConsole() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter new array length");
+        int newLength = scanner.nextInt();
+        this.array = new int[newLength];
+        for (int i = 0; i < newLength; i++) {
+            System.out.println("enter value of element with index " + i);
+            this.array[i] = scanner.nextInt();
+        }
+    }
+
+    public void fillArrayByFile(Path path) {
+        this.array = getDataFromFile(path);
+    }
+
+    public void fillArrayWithRandom(int newLength) {
+        this.array = new int[newLength];
+        for (int i = 0; i < newLength; i++) {
+            this.array[i] = (int) Math.random() % 30;
+        }
+    }
+
+    private int[] getDataFromFile(Path path) {
+        int[] fileArray = null;
+        try (Stream<String> lines = Files.lines(path)) {
+            fileArray = lines.mapToInt(Integer::parseInt).toArray();
+        } catch (IOException ignored) {
+        }
+
+        return fileArray;
     }
 
     @Override
@@ -223,5 +314,5 @@ public class Array {
 }
 
 interface Reporter {
-    boolean apear(int elem);
+    boolean appear(int elem);
 }
