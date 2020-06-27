952b7d8022b2e64c3d5c77314ea9c8acf7a8bcdf
Mishamba <misha.nenahov@gmail.com>
2020-06-27 08:05:09 +0300

    removed some more NotNull annotations

--- src/com/mishamba/day4/service/task2/JaggedArrayService.java
+++ src/com/mishamba/day4/service/task2/JaggedArrayService.java
@@ -1,9 +1,12 @@
 package com.mishamba.day4.service.task2;
 
-import org.jetbrains.annotations.NotNull;
+import com.mishamba.day4.exception.ProgramException;
 
 public class JaggedArrayService {
-    public int[][] sort(int[] @NotNull [] array, Marker larger) {
+    public int[][] sort(int[][] array, Marker larger) throws ProgramException {
+        if (array == null) {
+            throw new ProgramException("array is null");
+        }
         for (int i = 0; i < array.length; i++) {
             for (int j = i + 1; j < array.length; j++) {
                 if (compare(array[i], array[j], larger)) {
@@ -18,7 +21,7 @@ public class JaggedArrayService {
     }
 
     private boolean compare(int[] firstArray, int[] secondArray,
-                            @NotNull Marker expression) {
+                            Marker expression) {
         int firstMark = expression.calculate(firstArray);
         int secondMark = expression.calculate(secondArray);
 
--- src/com/mishamba/day4/service/task2/SortOptions.java
+++ src/com/mishamba/day4/service/task2/SortOptions.java
@@ -1,9 +1,12 @@
 package com.mishamba.day4.service.task2;
 
-import org.jetbrains.annotations.NotNull;
+import com.mishamba.day4.exception.ProgramException;
 
 public class SortOptions {
-    public static int largerSum(int @NotNull [] array) {
+    public static int largerSum(int [] array) throws ProgramException {
+        if (array == null) {
+            throw new ProgramException("array is null");
+        }
         int sum = 0;
         for (int elem : array) {
             sum += elem;
@@ -12,7 +15,10 @@ public class SortOptions {
         return sum;
     }
 
-    public static int maxElement(int @NotNull [] array) {
+    public static int maxElement(int [] array) throws ProgramException {
+        if (array == null) {
+            throw new ProgramException("array is null");
+        }
         int maxElement = 0;
         for (int elem : array) {
             if (maxElement < elem) {
@@ -23,7 +29,10 @@ public class SortOptions {
         return maxElement;
     }
 
-    public static int minElement(int @NotNull [] array) {
+    public static int minElement(int [] array) throws ProgramException {
+        if (array == null) {
+            throw new ProgramException("array is null");
+        }
         int minElement = Integer.MAX_VALUE;
         for (int elem : array) {
             if (minElement > elem) {
